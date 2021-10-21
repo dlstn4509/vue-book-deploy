d672920fbfca68f9a5ef2aa6cd06a575bfeca98f
dlstn4509 <dlstn7609@hanmail.net>
2021-10-21 11:24:58 +0900

    ..

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 const path = require('path')
 
 
-app.use('/', express.static(path.join(__dirname, './dist')));
+app.use('/', express.static(path.join(__dirname, './dist'))); 
 
 app.listen(3200, () => { console.log('http://127.0.0.1:3200') });
